c9b175df243eb47605838afb9c4fc6cfadcdd744
MihailKarataev <karataev@russia-israel.com>
2022-12-07 08:06:40 +0200

    html_first

---
+++ index.php
@@ -0,0 +1,199 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Smart</title>
+</head>
+<body>
+    <header class="header">
+        <div class="container">
+            <div class="logo"></div>
+            <div class="container__title">Smart Agency Landing Page</div>
+            <div class="container__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do.</div>
+            <div class="header__sendForm">
+                <div class="header__sendForm-title"></div>
+                <input type="text" name="name">
+                <input type="text" name="email">
+                <input type="text" name="text_message">
+                <submit>Send Message</submit>
+            </div>
+        </div>
+    </header>
+
+    <div class="workingProcess">
+        <div class="container">
+            <div class="container__title">Working Process</div>
+            <div class="container__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do.</div>
+            <div class="workingProcess__content">
+                <div class="workingProcess__item workingProcess__item-blue">
+                    <div class="workingProcess__item-image"></div>
+                    <div class="workingProcess__item-title"></div>
+                    <div class="workingProcess__item-text"></div>
+                    <div class="workingProcess__item-button"></div>
+                </div>
+                <div class="workingProcess__item workingProcess__item-purple">
+                    <div class="workingProcess__item-image"></div>
+                    <div class="workingProcess__item-title"></div>
+                    <div class="workingProcess__item-text"></div>
+                    <div class="workingProcess__item-button"></div>
+                </div>
+                <div class="workingProcess__item workingProcess__item-violet">
+                    <div class="workingProcess__item-image"></div>
+                    <div class="workingProcess__item-title"></div>
+                    <div class="workingProcess__item-text"></div>
+                    <div class="workingProcess__item-button"></div>
+                </div>
+                <div class="workingProcess__item workingProcess__item">
+                    <div class="workingProcess__item-image"></div>
+                    <div class="workingProcess__item-title"></div>
+                    <div class="workingProcess__item-text"></div>
+                    <div class="workingProcess__item-button"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="featuredWorks">
+        <div class="container">
+            <div class="container__title">Featured Works</div>
+            <div class="container__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do.</div>
+            <div class="featuredWorks__item">
+                <a heref="#"><img src=""></a>
+            </div>
+            <div class="featuredWorks__item">
+                <a heref="#"><img src=""></a>
+            </div>
+            <div class="featuredWorks__item">
+                <a heref="#"><img src=""></a>
+            </div>
+            <div class="featuredWorks__item">
+                <a heref="#"><img src=""></a>
+            </div>
+            <div class="featuredWorks__item">
+                <a heref="#"><img src=""></a>
+            </div>
+            <div class="featuredWorks__item">
+                <a heref="#"><img src=""></a>
+            </div>
+            <div class="featuredWorks__item">
+                <a heref="#"><img src=""></a>
+            </div>
+            <div class="featuredWorks__item">
+                <a heref="#"><img src=""></a>
+            </div>
+            <div class="featuredWorks__item">
+                <a heref="#"><img src=""></a>
+            </div>
+        </div>
+    </div>
+    <div class="serviceWeProvide">
+        <div class="container">
+            <div class="container__title">Service We Provide</div>
+            <div class="container__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do.</div>
+            <div class="serviceWeProvide__content">
+                <div class="serviceWeProvide__content-image-left"></div>
+                <div class="serviceWeProvide__content-title"></div>
+                <div class="serviceWeProvide__content-text"></div>
+                <div class="serviceWeProvide__content-image-right"></div>
+                <button class="serviceWeProvide__content-button">Know More</button>
+            </div>
+        </div>
+    </div>
+    <div class="teamCinenSkills">
+        <div class="container">
+            <div class="container__title">Team Chinen & Skills</div>
+            <div class="container__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do.</div>
+            <div class="teamCinenSkills__content">
+                <div class="teamCinenSkills__item">
+                    <div class="teamCinenSkills__description">
+                        <div class="teamCinenSkills__description-name">Bedi Manos</div>
+                        <div class="teamCinenSkills__description-function">UI/UX Desiginer</div>
+                    </div>
+                </div>
+                <div class="teamCinenSkills__item teamCinenSkills__item-active">
+                    <div class="teamCinenSkills-skills">
+                        <div class="teamCinenSkills-skills-design">UX/UI Desigin</div>
+                        <div class="teamCinenSkills-skills-language">English</div>
+                        <div class="teamCinenSkills-skills-communication">Communication</div>
+                    </div>
+                    <div class="teamCinenSkills__description">
+                        <div class="teamCinenSkills__description-name">Al Rayhan</div>
+                        <div class="teamCinenSkills__description-function">UI/UX Desiginer</div>
+                        <div class="teamCinenSkills__description-social">
+                            <a class="teamCinenSkills__description-social-tweeter" href=#></a>
+                            <a class="teamCinenSkills__description-social-facebook" href=#></a>
+                            <a class="teamCinenSkills__description-social-vk" href=#></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="teamCinenSkills__item">
+                    <div class="teamCinenSkills__description">
+                        <div class="teamCinenSkills__description-name">Ali Sayed</div>
+                        <div class="teamCinenSkills__description-function">UI/UX Desiginer</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="clientSaid">
+        <div class="container">
+            <div class="container__title">Client Said</div>
+            <div class="container__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do.</div>
+            <div class="clientSaid__content">
+                <div class="clientSaid__item">
+                    <div class="clientSaid__item-photo"></div>
+                    <div class="clientSaid__item-review"></div>
+                    <div class="clientSaid__item-stars"></div>
+                    <div class="clientSaid__item-signature">
+                        <div class="clientSaid__item-name"></div>
+                        <div class="clientSaid__item-function"></div>
+                    </div>
+                </div>
+                <div class="clientSaid__item">
+                    <div class="clientSaid__item-photo"></div>
+                    <div class="clientSaid__item-review"></div>
+                    <div class="clientSaid__item-stars"></div>
+                    <div class="clientSaid__item-signature">
+                        <div class="clientSaid__item-name"></div>
+                        <div class="clientSaid__item-function"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <footer>
+        <div class="container container__products">
+            <div class="product">
+                <img src="#" alt="">
+            </div>
+            <div class="product">
+                <img src="#" alt="">
+            </div>
+            <div class="product">
+                <img src="#" alt="">
+            </div>
+            <div class="product">
+                <img src="#" alt="">
+            </div>
+            <div class="product">
+                <img src="#" alt="">
+            </div>
+            <div class="product">
+                <img src="#" alt="">
+            </div>
+        </div>
+        <div class="container container__footer">
+            <button class="contactUs"></button>
+            <div class="footer__socials">
+                <div class="socials_facebook"></div>
+                <div class="socials_tweeter"></div>
+                <div class="socials_vk"></div>
+                <div class="copyright">2015-2016 Deviserweb Ltg.</div>
+            </div>
+        </div>
+    </footer>
+</body>
+</html>
